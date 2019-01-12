781da36918075a481f7829d16ed6f66d020bd917
samveldhuis <45663925+samveldhuis@users.noreply.github.com>
2019-01-12 15:09:13 +0100

    Add files via upload

---
+++ Frontent Development/script/script.js
@@ -0,0 +1,16 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+/*Invul naam*/
+
+var zoekknop = document.querySelector('.zoekImg');
+var uitschuif = document.querySelector('.zoeken');
+
+function show() {
+    uitschuif.classList.toggle('zichtbaar');
+}
+
+zoekknop.addEventListener('click', show);
