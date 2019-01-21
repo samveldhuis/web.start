34def7d9641fe05b12bf72b4c5105281cf225b2a
samveldhuis <45663925+samveldhuis@users.noreply.github.com>
2019-01-21 16:20:42 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,24 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+/*Invul naam*/
+
+var zoekknop = document.querySelector('.zoekImg');
+var uitschuif = document.querySelector('.zoeken');
+
+function show() {
+    uitschuif.classList.toggle('zichtbaar');
+}
+
+zoekknop.addEventListener('click', show);
+
+
+function downloadFunction() {
+  var bericht = document.getElementById("mijnPopup");
+  bericht.classList.toggle("show");
+}
+
+
